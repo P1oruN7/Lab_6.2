93b1cce904cce96feec487272aa61e9e96a12048
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-06-21 23:27:12 +0300

    123

--- Client/src/Common/Commands/Draw.java
+++ Client/src/Common/Commands/Draw.java
@@ -4,11 +4,11 @@ import Common.Command;
 import Common.Invoker;
 
 public class Draw implements Command {
+    private static final long serialVersionUID = 6529685098267757690L;
     public Draw(){
         Invoker.regist("draw",this);
     }
-
-
+    
     @Override
     public String getInfo(){return "draw: нарисовать случайного котика";}
 
