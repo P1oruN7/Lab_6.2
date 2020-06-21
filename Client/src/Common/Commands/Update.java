a1aed59db1c56a04eda07a9b43dc9973df7fc700
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-06-21 23:15:22 +0300

    123

--- Client/src/Common/Commands/Update.java
+++ Client/src/Common/Commands/Update.java
@@ -4,28 +4,53 @@ import Readers.*;
 import Routes.Coordinates;
 import Routes.Location;
 import Routes.Route;
+import Utility.*;
 
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.Map;
 
 /**
  * Команда  "ПРАВКИ!"
  */
 public class Update implements Command {
-    public Update(){
-        Invoker.regist("update",this);
-    }
+    static boolean changedName = false;  // показывает, было ли изменено имя элемента
+    static boolean changedCoordinates = false; // показывает, было ли изменено поле coordinates
+    static boolean changedFrom = false; //показывает, было ли изменено поле from
+    static boolean changedTo = false; // показывает, было ли изменено поле to
+    static boolean changedDist = false; // показывает, было ли изменено поле distance
+    private static final long serialVersionUID = 6529685098267757690L;
 
     /**
      * Метод для изменения значений элемента по id
      * @throws IOException
      */
     @Override
-        public  void execute (String s) throws IOException {
-//        while (s.equals("") || s == null){
-//            System.out.println("Введите ID элемента, который вы хотите апдейтнуть. Если хотите выйти из апдейтинга, введите -1.");
-//            s = Utility.ClientMain.reader.readLine();
-//            if(s. trim().equals("-1") | s. trim().equals("- 1")) return;
-//        }
+        public  void execute (String s) {
+                s = s.trim();
+                Long id = Checker.longChecker(s);
+                Route r = ServerMain.c.searchById(id);
+                if (r ==null) {
+                    ServerSender.send("похоже элемента с таким айди не существует",0);
+                    return;
+                }
+                int index =  ServerMain.c.Routes.indexOf(r);
+               ServerSender.send("Состояние элемента сейчас: " + ServerMain.c.Routes.get(index).toString(),0);
+               Route route = new Route();
+               route.setCreationDate(ServerMain.c.Routes.get(index).getCreationDate());
+               String s2 = new String( ServerReceiver.receive() );
+               String [] arrayOfStrings = s2.split(" ");
+               route.setId(id);
+               route.setName(arrayOfStrings[0]);
+               route.setCoordinates(new Coordinates(Integer.parseInt(arrayOfStrings[1]), Float.parseFloat(arrayOfStrings[2])));
+              if (!arrayOfStrings[3].equals("null") ) route.setFrom(new Location(Long.parseLong(arrayOfStrings[3]), Double.parseDouble(arrayOfStrings[4]), arrayOfStrings[5]));
+               route.setTo(new Location(Long.parseLong(arrayOfStrings[6]), Double.parseDouble(arrayOfStrings[7]), arrayOfStrings[8]));
+              if (!arrayOfStrings[9].equals("null") ) route.setDistance(Float.parseFloat(arrayOfStrings[9]));
+               Remove_by_id remove_by_id = new Remove_by_id();
+               remove_by_id.execute(s);
+               ServerMain.c.Routes.add(route);
+               ServerSender.send("\n" +"Вы достигли успеха в замене элемента по айди!",0);
     }
     @Override
     public String getInfo() {
