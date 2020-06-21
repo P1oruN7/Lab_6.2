befcc689fbeacef5cefd5a36495de5240382602b
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-06-21 23:25:55 +0300

    123

--- Server/src/Common/Commands/Remove_by_id.java
+++ Server/src/Common/Commands/Remove_by_id.java
@@ -2,6 +2,7 @@ package Common.Commands;
 import Common.*;
 import Readers.*;
 import Routes.Route;
+import Utility.ServerMain;
 
 /**
  * Команда "УДОЛИ!"
@@ -12,21 +13,19 @@ public class Remove_by_id implements Command {
      */
     @Override
     public void execute(String s){
-//    if (s2==null | s2.equals("")) {
-//        System.out.println("кажется вы забыли ввести айди");
-//        return;
-//    }
-//
-//       long removeId = Checker.longChecker(s2);
-//    Route r = c.searchById(removeId);
-//    if (r ==null) {System.out.println("похоже элемента с таким айди не существует");
-//        return;
-//    }
-//       c.Routes.remove(c.Routes.indexOf(r));
+    if (s==null | s.equals("")) {
+        System.out.println("кажется вы забыли ввести айди");
+        return;
+    }
+       long removeId = Checker.longChecker(s);
+    Route r = ServerMain.c.searchById(removeId);
+    if (r ==null) {System.out.println("похоже элемента с таким айди не существует");
+        return;
+    }
+       ServerMain.c.Routes.remove(ServerMain.c.Routes.indexOf(r));
     }
     @Override
     public String getInfo() {
         return "remove_by_id id : удалить элемент из коллекции по его id";
     }
-
 }
