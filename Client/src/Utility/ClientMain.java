871fcf13479b2c433e40bd1e6b51215c0adcce60
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-06-23 02:43:35 +0300

    Add files via upload

--- Client/src/Utility/ClientMain.java
+++ Client/src/Utility/ClientMain.java
@@ -1,25 +1,27 @@
 package Utility;
 
-import Common.CreatePerson;
-import Utility.ClientReceiver;
-import Utility.ClientSender;
 import Common.Invoker;
 import Common.Commands.*;
 import Common.Command;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.BindException;
 import java.net.DatagramSocket;
 import java.net.SocketTimeoutException;
-import java.nio.channels.DatagramChannel;
-import java.sql.SQLOutput;
 import java.util.Map;
 
+/**
+ * Главненький
+ */
 public class ClientMain {
     public static boolean work = true; // переменная, отвечающая за выход из программы. Как только она станет false, программа завершается
     public static BufferedReader reader = null;
+
+    /**
+     * psvm
+     * @param args аргументики
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
 
         Add add = new Add();
@@ -40,24 +42,23 @@ public class ClientMain {
         Update update = new Update();
 
         reader = new BufferedReader(new InputStreamReader(System.in));
-                DatagramSocket ds = new DatagramSocket();
-                ClientReceiver.sock = ds ;
-                ClientReceiver.clientport = ds.getLocalPort();
-            while (work) {
-                System.out.print("Введите команду:  ");
-                String s = reader.readLine();
-               Map<Command,String> commandparamMap= Invoker.execute(s);
-                if (commandparamMap!=null) {
-                    ClientSender.send(commandparamMap);
-                    try {
-                        ClientReceiver.receive();
-                    }
-                    catch (SocketTimeoutException e){
-                        System.out.println("Сервер не отвечает или занят,попробуйте ещё раз и убедитесь,что сервер работает.");
-                    }
+        DatagramSocket ds = new DatagramSocket();
+        ClientReceiver.sock = ds;
+        ClientReceiver.clientport = ds.getLocalPort();
+        while (work) {
+            System.out.print("Введите команду:  ");
+            String s = reader.readLine();
+            Map<Command, String> commandparamMap = Invoker.execute(s);
+            if (commandparamMap != null) {
+                ClientSender.send(commandparamMap);
+                try {
+                    ClientReceiver.receive();
+                } catch (SocketTimeoutException e) {
+                    System.out.println("Сервер не отвечает или занят,попробуйте ещё раз и убедитесь,что сервер работает.");
                 }
             }
         }
     }
+}
 
 
--- Client/src/Utility/ClientReceiver.java
+++ Client/src/Utility/ClientReceiver.java
@@ -3,15 +3,22 @@ package Utility;
 import java.io.*;
 import java.net.*;
 import java.util.Map;
-
+/**
+ * Получатель
+ */
 public class ClientReceiver {
-   public  static DatagramSocket sock;
+    public static DatagramSocket sock;
     public static int clientport;
-    public static Map<String,Integer> answer;
+    public static Map<String, Integer> answer;
+
+    /**
+     * Получить
+     * @return массивчик байтиков
+     */
     public static void receive() throws SocketTimeoutException {
         byte[] buffer = new byte[1000000];
         try {
-            DatagramPacket reply = new DatagramPacket(buffer,buffer.length);
+            DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
             sock.setSoTimeout(5000);
             sock.receive(reply);
             ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(reply.getData());
@@ -20,23 +27,21 @@ public class ClientReceiver {
             byteArrayInputStream.close();
             objectInputStream.close();
             answer = (Map<String, Integer>) obj;
-            if (answer.entrySet().iterator().next().getValue()==0) {
-                System.out.println("Ответ с сервера: "+answer.entrySet().iterator().next().getKey());
-            }
-            else if (answer.entrySet().iterator().next().getValue() == 1){
-                System.out.println("Ответ с сервера: "+answer.entrySet().iterator().next().getKey());
+            if (answer.entrySet().iterator().next().getValue() == 0) {
+                System.out.println("Ответ с сервера: " + answer.entrySet().iterator().next().getKey());
+            } else if (answer.entrySet().iterator().next().getValue() == 1) {
+                System.out.println("Ответ с сервера: " + answer.entrySet().iterator().next().getKey());
                 BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
                 String s = reader.readLine();
                 ClientSender.send(s);
                 ClientReceiver.receive();
-            }
-            else if (answer.entrySet().iterator().next().getValue() == 2){
-                System.out.println("Ответ с сервера: "+answer.entrySet().iterator().next().getKey());
+            } else if (answer.entrySet().iterator().next().getValue() == 2) {
+                System.out.println("Ответ с сервера: " + answer.entrySet().iterator().next().getKey());
                 ClientReceiver.receive();
             }
         } catch (SocketException e) {
             e.printStackTrace();
-        } catch (SocketTimeoutException e){
+        } catch (SocketTimeoutException e) {
             System.out.println("Возможно сервер занят или выключен,попробуйте ещё раз.");
         } catch (IOException e) {
             e.printStackTrace();
