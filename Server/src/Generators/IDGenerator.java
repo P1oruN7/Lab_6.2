002f2f2bcc0e2c0443429c801d1bb1fe34fda76f
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-06-23 02:48:34 +0300

    Add files via upload

--- Server/src/Generators/IDGenerator.java
+++ Server/src/Generators/IDGenerator.java
@@ -6,9 +6,10 @@ package Generators;
 public class IDGenerator {
     /**
      * Метод для создания id
+     *
      * @return id (Long)
      */
-    public static long generateNewID(){
+    public static long generateNewID() {
         return (long) (1 + Math.random() * (Long.MAX_VALUE - 1));
     }
 
