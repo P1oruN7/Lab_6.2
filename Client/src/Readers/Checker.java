262ed5b2a3ded793c0fe31d742daff772ae7e1fe
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-06-23 02:44:51 +0300

    Add files via upload

--- Client/src/Readers/Checker.java
+++ Client/src/Readers/Checker.java
@@ -7,16 +7,18 @@ package Readers;
 public class Checker {
     /**
      * Проверка на Integer
+     *
      * @param s строка
      * @return число (int) или null
      */
-    public static  Integer intChecker (String s) {
-        if (s.equals("") || s == null ) {
+    public static Integer intChecker(String s) {
+        if (s.equals("") || s == null) {
             System.out.println("Строка не читается или пустая. Попробуйте ещё разок, уверен, в следующий раз обязательно получится!");
-            return null;}
+            return null;
+        }
         try {
             return Integer.parseInt(s);
-        }catch (Exception e) {
+        } catch (Exception e) {
             return null;
         }
     }
@@ -24,32 +26,37 @@ public class Checker {
 
     /**
      * Проверка на Long
+     *
      * @param s строка
      * @return число (long) или null
      */
-    public static Long longChecker (String s){
-        if ( s.equals("") || s == null) {
+    public static Long longChecker(String s) {
+        if (s.equals("") || s == null) {
             System.out.println("Строка не читается или пустая. Попробуйте ещё разок, уверен, в следующий раз обязательно получится!");
-            return  null;}
+            return null;
+        }
         try {
             return Long.parseLong(s);
-        }catch (Exception e) {
+        } catch (Exception e) {
             return null;
         }
 
     }
+
     /**
      * Проверка на Float
+     *
      * @param s строка
      * @return число (float) или null
      */
-    public static Float floatChecker (String s){
-        if ( s.equals("") || s == null) {
+    public static Float floatChecker(String s) {
+        if (s.equals("") || s == null) {
             System.out.println("Строка не читается или пустая. Попробуйте ещё разок, уверен, в следующий раз обязательно получится!");
-            return  null;}
+            return null;
+        }
         try {
             return Float.parseFloat(s);
-        }catch (Exception e) {
+        } catch (Exception e) {
             return null;
         }
 
@@ -57,16 +64,18 @@ public class Checker {
 
     /**
      * Проверка на Double
+     *
      * @param s строка
      * @return число (double) или null
      */
-    public static Double doubleChecker (String s){
-        if ( s.equals("") || s == null) {
+    public static Double doubleChecker(String s) {
+        if (s.equals("") || s == null) {
             System.out.println("Строка не читается или пустая. Попробуйте ещё разок, уверен, в следующий раз обязательно получится!");
-            return  null;}
+            return null;
+        }
         try {
             return Double.parseDouble(s);
-        }catch (Exception e) {
+        } catch (Exception e) {
             return null;
         }
 
--- Client/src/Readers/FileSourceReader.java
+++ Client/src/Readers/FileSourceReader.java
@@ -15,19 +15,20 @@ public class FileSourceReader extends Reader {
             File script = new File(path);
             this.bufferedReader = new BufferedReader(new FileReader(path));
         } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());;
+            System.out.println(e.getMessage());
+            ;
         }
     }
 
     @Override
-    public String getLine(){
+    public String getLine() {
         try {
             String s = bufferedReader.readLine();
-            if (s!= null){
-                System.out.print(s + "\n");
-            }
+//            if (s!= null){
+//                System.out.print(s + "\n");
+//            }
             return s;
-        } catch(IOException e){
+        } catch (IOException e) {
             System.out.println("Ввод неожиданно прервался");
             return null;
         }
