ddee1870d0a731123215c2c4bc0a47ed135b5b95
оля <61423764+Olya-droid@users.noreply.github.com>
2020-06-28 03:03:05 +0500

    Update Average_of_distance.java

--- Server/src/Common/Commands/Average_of_distance.java
+++ Server/src/Common/Commands/Average_of_distance.java
@@ -14,20 +14,18 @@ public class Average_of_distance implements Command {
     /**
      * Метод для вывода среднего значения поля distance для всех элементов коллекции
      */
-    @Override
+   @Override
     public void execute(String s) {
         if (ServerMain.c.Routes.size() > 0) {
-            Float sum = 0f;
-            int countOfNull = 0;
-            for (Routes.Route r : ServerMain.c.Routes) {
-                if (r.getDistance() != null)
-                    sum += r.getDistance();
-                else
-                    countOfNull++;
-            }
-            if (ServerMain.c.Routes.size() - countOfNull > 0)
-                ServerSender.send("\n \nСреднее значение distance: " + sum / (ServerMain.c.Routes.size() - countOfNull) + "\n \n", 0);
-
+            Double averageOfDistance = ServerMain.c.Routes.stream()
+                    .map(Route::getDistance)
+                    .mapToDouble( (t) -> {
+                        if ((t)==null) (t) = Float.valueOf(0);
+                        return (t);
+                    })
+                    .average()
+                    .getAsDouble();
+            ServerSender.send("\n \nСреднее значение distance: " +averageOfDistance + "\n \n", 0);
         } else
             ServerSender.send("\n \n Коллекция пуста как банка кофе, купленая в начале выполнения этой работы \n \n", 0);
     }
