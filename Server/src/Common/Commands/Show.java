d33e6f5b66d0d6e3b678a5533f4ba7567284007e
оля <61423764+Olya-droid@users.noreply.github.com>
2020-06-28 18:09:11 +0500

    Update Show.java

--- Server/src/Common/Commands/Show.java
+++ Server/src/Common/Commands/Show.java
@@ -19,12 +19,10 @@ public class Show implements Command {
         if (ServerMain.c.Routes.size() == 0) {
             ServerSender.send("\n\nКоллекция пуста, милорд\n\n", 0);
         } else {
-            String string = "\n";
-            for (Route r : ServerMain.c.Routes) {
-                string += "  " + r.toString() + "\n";
-            }
-            ServerSender.send(string, 0);
-
+            String[] show = {"\n"};
+            ServerMain.c.Routes.stream()
+                    .forEachOrdered(x -> show[0] += x.toString() + "\n");
+            ServerSender.send(show[0], 0);
         }
     }
 
