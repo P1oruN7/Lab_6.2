7fd29add2308ef851886f2a9f1805362470469b6
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-06-28 14:05:32 +0300

    Update ServerMain.java

--- Server/src/Utility/ServerMain.java
+++ Server/src/Utility/ServerMain.java
@@ -57,34 +57,34 @@ public class ServerMain {
       //  CreateServer.create();
        // System.out.println("Сервер запущен.");
        ConsoleSourceReader bufferReader = new ConsoleSourceReader();///////
-//        String path = null;
-//
-//        try {
-//            path = args[0];
-//            c = XmlReader.getCollection(path);
-//        } catch (ArrayIndexOutOfBoundsException ignored) {
-//        }
-//        while (c == null) {
-//            System.out.println("Введите расположение файла с коллекцией или нажмите Enter, чтобы начать работу с дефолтной коллекцией: ");
-//            path = bufferReader.getLine() + "";
-//            if (path.equals("")) {
-//                path = "resources/input.xml";
-//                System.out.println("Вы начали работу с коллекцией по умолчанию. Если хотите увидеть ее элементы, введите \"show\"");
-//            }
-//
-//            c = XmlReader.getCollection(path);
-//
-//        }
-//
-//        try {
-//            if (new File(path).exists()) {
-//                c.setPath(path);
-//            }
-//        } catch (NullPointerException ignored) {
-//        }  //////////
+        String path = null;
+
+        try {
+            path = args[0];
+            c = XmlReader.getCollection(path);
+        } catch (ArrayIndexOutOfBoundsException ignored) {
+        }
+        while (c == null) {
+            System.out.println("Введите расположение файла с коллекцией или нажмите Enter, чтобы начать работу с дефолтной коллекцией: ");
+            path = bufferReader.getLine() + "";
+            if (path.equals("")) {
+                path = "resources/input.xml";
+                System.out.println("Вы начали работу с коллекцией по умолчанию. Если хотите увидеть ее элементы, введите \"show\"");
+            }
+
+            c = XmlReader.getCollection(path);
+
+        }
+
+        try {
+            if (new File(path).exists()) {
+                c.setPath(path);
+            }
+        } catch (NullPointerException ignored) {
+        }  //////////
 
 
-           c = new Collection(); // !!!!
+//           c = new Collection(); // !!!!
         boolean serverCreated = false;
         while (!serverCreated) {
             System.out.print("Введите порт:  ");
